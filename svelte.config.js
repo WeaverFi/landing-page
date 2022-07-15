a1018469207c1614ded1cbbcc791b9d131060bbb
Ncookiez <ncukiert@gmail.com>
2022-07-15 19:19:10 -0300

    Rollback to Adapter-Auto

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,11 @@
-import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: preprocess(),
 	kit: {
-		adapter: adapter({ edge: true })
+		adapter: adapter()
 	}
 };
 
