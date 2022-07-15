a3af5531a7e7e9920c8647318f8e75c276fad2a0
Ncookiez <ncukiert@gmail.com>
2022-07-15 19:47:32 -0300

    Rolled Back (Was Cache Issue)

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,11 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-netlify';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: preprocess(),
 	kit: {
-		adapter: adapter()
+		adapter: adapter({ edge: true })
 	}
 };
 
