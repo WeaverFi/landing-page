8bb5a3dfbbfdc0fe45d5fa1a47ff0d6bb60219cf
Ncookiez <ncukiert@gmail.com>
2023-01-10 21:06:11 -0300

    Package Bumps

--- src/lib/TokenWeb.svelte
+++ src/lib/TokenWeb.svelte
@@ -1,4 +1,4 @@
-<script type="ts">
+<script lang="ts">
 
   // Imports:
   import { browser } from "$app/environment";
--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,9 @@
 import adapter from '@sveltejs/adapter-netlify';
-import preprocess from 'svelte-preprocess';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: preprocess(),
+	preprocess: vitePreprocess(),
 	kit: {
 		adapter: adapter()
 	}
